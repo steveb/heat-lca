085151db7b81796f4ecd9621a730396a879c61e8
Steve Baker <sbaker@redhat.com>
2014-12-23 11:12:50 +1300

    reveal.js-2.6.2 unmodified

--- scripts/fetch-requirements.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-rm -rf files
-mkdir -p files
-pushd files
-wget http://download.wikimedia.org/mediawiki/1.20/mediawiki-1.20.2.tar.gz
-git clone git://github.com/steveb/SwiftCloudFiles.git
-popd
